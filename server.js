a16704d60f369fbc6fa77b78ca8180b286221a77
r616on <r616on@gmail.com>
2021-08-12 00:58:34 +0300

    done

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const Router = require("koa-router");
 const router = new Router();
 const uuid = require("uuid");
 
+const WS = require("ws");
+
 app.use(
   koaBody({
     urlencoded: true,
@@ -19,49 +21,126 @@ app.use(
 );
 app.use(cors());
 
-router.get("/contacts", async (ctx, next) => {
-  // return list of contacts
-  ctx.response.body = contacts;
-});
+// router.get("/contacts", async (ctx, next) => {
+//   // return list of contacts
+//   ctx.response.body = contacts;
+// });
+// router.post("/login", async (ctx, next) => {
+//   // create new contact
+//   const data = JSON.parse(ctx.request.body);
+//   const index = clients.findIndex((item) => {
+//     if (item.login === data.login) {
+//       return true;
+//     }
+//   });
+//   if (index > -1) {
+//     ctx.response.status = 205;
+//   } else {
+//     ctx.response.status = 204;
+//     clients.push({ login: data.login });
+//   }
 
-router.post("/cont", async (ctx, next) => {
-  // create new contact
-  console.log("post");
-  console.log(ctx.request.body);
-  ctx.response.body = JSON.stringify({ ok: true });
-  // contacts.push({ ...ctx.request.body, id: uuid.v4() });
-  ctx.response.status = 204;
-});
-
-router.delete("/contacts/:id", async (ctx, next) => {
-  // remove contact by id (ctx.params.id)
-  const index = contacts.findIndex(({ id }) => id === ctx.params.id);
-  if (index !== -1) {
-    contacts.splice(index, 1);
-  }
-  ctx.response.status = 204;
-});
+// });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const server = http.createServer(app.callback()).listen(port);
+const server = http.createServer(app.callback());
 
-// app.use(async (ctx) => {
-//   console.log("Serv RUN");
+const wsServer = new WS.Server({ server });
+const clients = [];
+const messageDb = [];
+//{ id: 123235, login: "ivan", ws: "" }
 
-//   if (ctx.request.method === "POST") {
-//     const data = JSON.parse(ctx.request.body);
-//     switch (data.method) {
-//       case "ok":
-//         ctx.response.body = { ok: true };
-//         break;
-//       default:
-//         ctx.response.status = 404;
-//         break;
-//     }
-//   } else {
-//     ctx.response.status = 404;
-//     return;
-//   }
-// });
+wsServer.on("connection", (ws, req) => {
+  const id = uuid.v4();
+  clients.push({ id: id, login: "", ws: ws });
+  let name = "";
+
+  ws.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    //Registr chat
+    if (message.method === "register") {
+      name = message.login;
+      const index = clients.findIndex((item) => {
+        if (item.login === message.login) {
+          return true;
+        }
+      });
+      if (index > -1) {
+        ws.send(JSON.stringify({ method: "register", status: "no" }));
+      } else {
+        clients.forEach((item) => {
+          if (item.id === id) {
+            item.login = name;
+          }
+        });
+        ws.send(
+          JSON.stringify({ method: "register", status: "ok", name: name })
+        );
+        //update User
+        clients.forEach((user) => {
+          if (user.login === "") {
+          } else {
+            const arr = clients.map((el) => el.login);
+            const messSend = {
+              method: "update",
+              arrUser: arr,
+            };
+
+            user.ws.send(JSON.stringify(messSend));
+          }
+        });
+      }
+    }
+    //end update
+
+    // end registr chat
+
+    if (message.method === "message") {
+      let moment = require("moment");
+      const date = moment().format("lll");
+      messageDb.push({
+        author: name,
+        textMassage: message.textMassage,
+        date: date,
+      });
+      //Newsletter to users
+      clients.forEach((user) => {
+        if (user.login === "") {
+        } else {
+          const messSend = {
+            author: name,
+            textMassage: message.textMassage,
+            method: "message",
+            date: date,
+          };
+
+          user.ws.send(JSON.stringify(messSend));
+        }
+      });
+      //end newsletter
+    }
+  });
+
+  ws.on("close", () => {
+    const index = clients.findIndex((item) => {
+      if (item.login === name) {
+        return true;
+      }
+    });
+    clients.splice(index, 1);
+    const arrName = clients.map((item) => item.login);
+    clients.forEach((user) => {
+      if (user.login === "") {
+      } else {
+        const messSend = {
+          method: "update",
+          arrUser: arrName,
+        };
+        user.ws.send(JSON.stringify(messSend));
+      }
+    });
+  });
+});
+server.listen(port);
